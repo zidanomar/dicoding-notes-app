e919d1e0f636e11188e097c770fcdc0a7d14caac
Fake Koji <zidankoji77@gmail.com>
2021-05-21 17:11:29 +0300

    notes app initial commit

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const http = require('http');
+const Hapi = require('@hapi/hapi');
+const routes = require('./src/routes');
 
-const requestListener = (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.statusCode = 200;
-  res.end('<h1>Halo HTTP Server!</h1>');
-}
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['http://notesapp-v1.dicodingacademy.com'],
+      },
+    },
+  });
 
-const server = http.createServer(requestListener);
-const port = 5000;
-const host = 'localhost';
+  server.route(routes);
 
-server.listen(port, host, () => {
-  console.log(`Server berjalan pada http://${host}:${port}`);
-})
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
